3088314745933955453858b4f444aa4dc350ba2a
mrleidesen <mrleidesen@outlook.com>
2021-05-31 14:14:07 +0800

    feat: add form

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import './assets/tailwind.css'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
 createApp(App)
   .use(router)
+  .use(Antd)
   .mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,38 @@
 <template>
   <div class="container mx-auto flex">
-    <div class="w-1/2 flex flex-col items-center box-border p-2">
-      <div 
+    <div class="flex flex-col items-center box-border p-2">
+      <a-card 
         v-for="(config, i) in configes"
         :key="i"
-        class="w-1/2 bg-white rounded-md overflow-hidden shadow flex flex-col box-border p-3 select-none cursor-pointer"
+        :title="config.name" 
+        class="w-96"
+        hoverable
+        @click="onChangeActive(i)"
       >
-        <h3 class="font-semibold text-gray-700 text-lg mb-2">{{ config.name }}</h3>
-        <div 
-          v-if="config.rurl" 
-          class="flex items-center text-xs mb-1"
-        >
-          <span>接口地址：</span>
-          <span class="bg-green-500 text-white p-1 rounded">{{ config.rurl }}</span>
-        </div>
-        <div 
-          v-if="config.rtype" 
-          class="flex items-center text-xs mb-1"
-        >
-          <span>请求类型：</span>
-          <span class="bg-blue-500 text-white p-1 rounded">{{ config.rtype.toUpperCase() }}</span>
-        </div>
-      </div>
+        <p>请求URL：<a-tag color="green">{{ config.rurl }}</a-tag></p>
+        <p v-if="config.rtype">请求类型：<a-tag color="blue">{{ config.rtype.toUpperCase() }}</a-tag></p>
+      </a-card>
     </div>
     <div class="flex-1 flex box-border p-2 border-l border-gray-200">
-
+      <div class="w-full box-border p-2 bg-white shadow-sm">
+        <a-form
+          :model="formState"
+          :label-col="{ span: 2 }"
+        >
+          <a-form-item label="名称" required>
+            <a-input v-model:value="formState.name"></a-input>
+          </a-form-item>
+          <a-form-item label="请求URL" required>
+            <a-input v-model:value="formState.rurl"></a-input>
+          </a-form-item>
+        </a-form>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 
 export default {
   setup() {
@@ -41,9 +43,23 @@ export default {
         rtype: 'get',
       }
     ])
+    const formState = reactive({
+      name: "",
+      rurl: "",
+      rtype: null
+    })
+    const activeIndex = ref(-1)
+
+    const onChangeActive = (index) => {
+      Object.assign(formState, configes[index])
+      activeIndex.value = index
+    }
 
     return {
-      configes
+      configes,
+      formState,
+      activeIndex,
+      onChangeActive,
     }
   }
 }
